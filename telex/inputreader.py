bf7c3d74a536f263bc6539b848b5351bae86cbbf
Ulrike Engeln <ulrike.engeln@tuhh.de>
2026-09-24 15:10:00 +0200

    time must not be the first column

--- telex/inputreader.py
+++ telex/inputreader.py
@@ -7,12 +7,13 @@ def readtracefile(filename):
     float_cols = [c for c in df_test if df_test[c].dtype == "float64"]
     float32_cols = {c: np.float32 for c in float_cols}
     data = pd.read_csv(filename, engine='c', dtype=float32_cols)
-    if data.columns[0]!='time':
-        print("The name of first column of trace in {} must be \'time\'. Can't read this trace. Quitting ....\n".format(filename))
+    if not 'time' in data.columns:
+        print("The column \'time\'must exist in the columns of trace {}. Can't read this trace. Quitting ....\n".format(filename))
         quit()
     data1 = {}
     for colname in data:
         data1[colname] = {}
         for i in range(0,len(data)):
             data1[colname][data["time"][i]] = data[colname][i]
-    return data1
+    # return data1
+    return data
